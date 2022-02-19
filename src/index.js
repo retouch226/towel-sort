3846d622d3ca53af03fd7fc7ecf26be6609975f5
Ivan <retouch226@gmail.com>
2022-02-19 15:21:03 +0200

    feat: 8/8

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+module.exports = function towelSort(matrix) {
+    if (matrix != null) {
+        let array = [];
+        for (let i = 0; i < matrix.length; i++) {
+            if (i % 2 === 0) {
+                for (j = 0; j < matrix[i].length; j++) {
+                    array.push(matrix[i][j]);
+                }
+            }
+            if (i % 2 !== 0) {
+                for (j = matrix[i].length - 1; j >= 0; j--) {
+                    array.push(matrix[i][j]);
+                }
+            }
+        }
 
-// You should implement your task here.
+        return array;
+    }
+    return [];
+};
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+//console.log(towelSort(matrix));
